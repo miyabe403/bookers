86688de4ff312b385c8e6e6457f7e82af2e43bdc
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-08-04 08:21:02 +0000

    [Add] ルーティングの記述変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  get 'homes/top'
+  # get 'books/new'
+  root to: 'homes#top'
+  post 'books' => 'books#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
