63ae0c420d4fcf6401e7bbb0f0611eec8cffa257
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-08-04 10:04:54 +0000

    [Add] bookのindexページの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,7 @@ class BooksController < ApplicationController
     @book = Book.new
   end
   
-  def create 
+  def create
     # １.&2. データを受け取り新規登録するためのインスタンス作成
     @book = Book.new(book_params)
     if @book.save  # 3. データが入力されていればデータをデータベースに保存するためのsaveメソッド実行
