87813e8f7926d5f19fca68f273525f81681b7390
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-08-05 00:03:35 +0000

    [Fix] トップ画面(root_path)に一覧ページへのリンクが表示

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,8 @@
 class HomesController < ApplicationController
   def top
   end
+  
+  def index
+    @books = Book.all  # allメソッドを使ってデータベース内のデータを全て取得する
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   # get '/books', to: 'books#new', as: 'books'
-  resources :books  # resourcesメソッドは、ルーティングを一括して自動生成してくれる機能です。
+  # resources :books  # resourcesメソッドは、ルーティングを一括して自動生成してくれる機能です。
+  # get '/books/index' => 'books#index'
+  
+  get 'books/new'
+  post 'books' => 'books#create'
+  get 'books' => 'books#index'
+  # .../lists/1 や .../lists/3 に該当する
+  get 'books/:id' => 'books#show', as: 'book'  # 名前付きルート as:オプションを追加「'lists#show'の設定を、listとして利用できる」
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'  # URLに:idを用いて記述すると、lists/◯◯/editのすべてのURLが対象になります。
+  patch 'books/:id' => 'books#update', as: 'update_book'  # 更新の場合はPATCHで指定します。
+  delete 'books/:id' => 'books#destroy', as: 'destroy_list'  # HTTPメソッドはDELETE、URLはlists/:idです。名前付きパスは、destroy_listと設定します。
 end
